9e9175dcdf67429ee64e71ae199fe0c4f348466b
hoBabu <amansahkumar004@gmail.com>
2023-12-08 12:50:31 +0530

    data types

---
+++ data_type.js
@@ -0,0 +1,17 @@
+/*
+number 
+string => ""
+boolean = true or false 
+bigint  for big numbers 
+symbol --> uniqueness 
+*/
+
+// undefined 
+let num ;
+console.log(num);  // output --> undefinded -->  value assign nahi hua hai 
+check;
+console.log(check); // check is not defined 
+
+// define and not defined both are different 
+
+// null --> standalone value 
