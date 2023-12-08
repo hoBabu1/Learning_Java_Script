855acb6593b991ab74349b34442676765dbe9b07
hoBabu <amansahkumar004@gmail.com>
2023-12-08 12:31:12 +0530

    Learning var , constant , let , console.table

---
+++ let_var_cons.js
@@ -0,0 +1,10 @@
+let myName = "hoBabu"
+const age = 99 ;
+var  college = "GEC "
+console.table([age , college , myName]); // use to print multiple at once 
+
+/*
+   const value cant be changed 
+   var --> we dont prefer it much bcz of issue related to scope 
+   let --> can be modified and scope logic is applicable 
+*/
